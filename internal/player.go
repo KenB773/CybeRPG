1b35b03cc101b6508f4699b38266c86442bf1c03
Ken B <160813236+KenB773@users.noreply.github.com>
2025-04-19 21:40:32 -0400

    Update player.go

--- internal/player.go
+++ internal/player.go
@@ -1,5 +1,5 @@
 // Player XP and stats - I'm nobody's pro game dev, so I just have everything in engine.go - but this could be used to keep things separate.
-// package internal
+package internal
 
 // type Player struct {
 //	Name string
