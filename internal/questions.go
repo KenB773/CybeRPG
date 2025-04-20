87c7fdbac36e7903e40d8f2e58ca248f2a307fb0
Ken B <160813236+KenB773@users.noreply.github.com>
2025-04-19 21:39:50 -0400

    Update questions.go

--- internal/questions.go
+++ internal/questions.go
@@ -1,15 +1,15 @@
-// Question bank storage
-package internal
+// Question bank storage - I'm not a pro dev and it was simpler to just keep everything in Engine.go - but this can be used for question sets in the future.
+// package internal
 
 // Placeholder for question bank
-var Questions = []Question{
-	{Category: "OSINT", Prompt: "What tool can you use to find a domain's WHOIS information?", Answer: "whois"},
-	{Category: "Incident Response", Prompt: "What port does SSH use by default?", Answer: "22"},
+// var Questions = []Question{
+// 	{Category: "OSINT", Prompt: "What tool can you use to find a domain's WHOIS information?", Answer: "whois"},
+//	{Category: "Incident Response", Prompt: "What port does SSH use by default?", Answer: "22"},
 	// 48 more questions to be added
-}
+// }
 
-type Question struct {
-	Category string
-	Prompt   string
-	Answer   string
-}
+// type Question struct {
+//	Category string
+//	Prompt   string
+//	Answer   string
+// }
